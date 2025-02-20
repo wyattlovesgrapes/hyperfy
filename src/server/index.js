1f5a0b8fc5e17084aec3149075aeb6f0fdeb2f0a
Ash <hello@ashconnell.com>
2025-02-20 21:56:17 +1100

    fix: remove artificial 2s delay on uploads… my bad :/

--- src/server/index.js
+++ src/server/index.js
@@ -83,8 +83,7 @@ fastify.get('/env.js', async (req, reply) => {
 
 fastify.post('/api/upload', async (req, reply) => {
   // console.log('DEBUG: slow uploads')
-  await new Promise(resolve => setTimeout(resolve, 2000))
-
+  // await new Promise(resolve => setTimeout(resolve, 2000))
   const file = await req.file()
   const ext = file.filename.split('.').pop().toLowerCase()
   // create temp buffer to store contents
